974ecb4d6e6991bedc120f58b9c89f053aee9abb
Dina Lamdany <dinalamdany@gmail.com>
2011-12-24 13:05:44 -0500

    Now returns tweet text

--- main.rb
+++ main.rb
@@ -5,19 +5,22 @@ require 'rubygems'
 require 'json'
 require 'net/http'
 
+#Function to get most recent tweet of a user
 def get_tweet(username)
   base_url = "http://api.twitter.com/1/users/show.json?screen_name=#{username}"
+  #Request the text from the URL
   resp = Net::HTTP.get_response(URI.parse(base_url))
   data = resp.body
   result = JSON.parse(data)
-
-  if result.has_key? 'Error'
+	
+	if result.has_key? 'Error'
     raise "Something went horribly wrong error"
   end
-
-  return result
+  
+	status = result['status']['text']
+  return status
 end
-
+	
 get '/' do
 	get_tweet("kylehardgrave")
 end
